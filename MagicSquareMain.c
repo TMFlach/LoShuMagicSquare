0e84fbd1fefa65b029adaf5242372e2c843cee32
Timothy Flach <tmflach@cpp.edu>
2022-10-23 20:52:24 -0700

    Initial commit with program outline

---
+++ MagicSquareMain.c
@@ -0,0 +1,9 @@
+
+
+//create main function for Lo Shu Magic Square
+
+//create array shuffler function (It will be assumed that an 3x3 with values 1-9 will be created in main beforehand)
+
+//create function to test whether an array is a Lo Shu Magic Square
+
+//create swap function for array shuffle function.
